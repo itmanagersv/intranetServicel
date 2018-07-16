8dc938ca2725fd730932ee407882108a12179a36
lguille1991 <lguille1991@gmail.com>
2018-07-16 17:19:02 -0600

    Se comentó card de otros deptos en el index

--- plantilla/plantillaIndex.php
+++ plantilla/plantillaIndex.php
@@ -331,30 +331,30 @@ class plantilla{
                                     </div><!--end class card-body -->
                                 </div>
                                 </div>
-                                <div class="card">
-                                    <div class="card-header">
-                                        <a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo">
-                                            Collapsible Group Item #2
-                                        </a>
-                                    </div>
-                                    <div id="collapseTwo" class="collapse" data-parent="#accordion">
-                                        <div class="card-body">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <div class="card-header">
-                                        <a class="collapsed card-link" data-toggle="collapse" href="#collapseThree">
-                                            Collapsible Group Item #3
-                                        </a>
-                                    </div>
-                                    <div id="collapseThree" class="collapse" data-parent="#accordion">
-                                        <div class="card-body">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                        </div>
-                                    </div>
-                                </div><!--end class card-->
+                                <!--div class="card"-->
+                                    <!--div class="card-header"-->
+                                        <!--a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo"-->
+                                            
+                                        <!--/a-->
+                                    <!--/div-->
+                                    <!--div id="collapseTwo" class="collapse" data-parent="#accordion"-->
+                                        <!--div class="card-body"-->
+                                            
+                                        <!--/div-->
+                                    <!--/div-->
+                                <!--/div-->
+                                <!--div class="card"-->
+                                    <!--div class="card-header"-->
+                                        <!--a class="collapsed card-link" data-toggle="collapse" href="#collapseThree"-->
+                                            
+                                        <!--/a-->
+                                    <!--/div-->
+                                    <!--div id="collapseThree" class="collapse" data-parent="#accordion"-->
+                                        <!--div class="card-body"-->
+                                            
+                                        <!--/div-->
+                                    <!--/div-->
+                                <!--/div--><!--end class card-->
                         </div>
                     </div>
                 </section>
@@ -655,30 +655,30 @@ class plantilla{
                                     </div><!--end class card-body -->
                                 </div>
                                 </div>
-                                <div class="card">
-                                    <div class="card-header">
-                                        <a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo">
-                                            Collapsible Group Item #2
-                                        </a>
-                                    </div>
-                                    <div id="collapseTwo" class="collapse" data-parent="#accordion">
-                                        <div class="card-body">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card">
-                                    <div class="card-header">
-                                        <a class="collapsed card-link" data-toggle="collapse" href="#collapseThree">
-                                            Collapsible Group Item #3
-                                        </a>
-                                    </div>
-                                    <div id="collapseThree" class="collapse" data-parent="#accordion">
-                                        <div class="card-body">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                        </div>
-                                    </div>
-                                </div><!--end class card-->
+                                <!--div class="card"-->
+                                    <!--div class="card-header"-->
+                                        <!--a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo"-->
+                                            
+                                        <!--/a-->
+                                    <!--/div-->
+                                    <!--div id="collapseTwo" class="collapse" data-parent="#accordion"-->
+                                        <!--div class="card-body"-->
+                                            
+                                        <!--/div-->
+                                    <!--/div-->
+                                <!--/div-->
+                                <!--div class="card"-->
+                                    <!--div class="card-header"-->
+                                        <!--a class="collapsed card-link" data-toggle="collapse" href="#collapseThree"-->
+                                            
+                                        <!--/a-->
+                                    <!--/div-->
+                                    <!--div id="collapseThree" class="collapse" data-parent="#accordion"-->
+                                        <!--div class="card-body"-->
+                                            
+                                        <!--/div-->
+                                    <!--/div-->
+                                <!--/div--><!--end class card-->
                         </div>
                     </div>
                 </section>
@@ -914,30 +914,30 @@ class plantilla{
                                         </div><!--end class card-body -->
                                     </div>
                                     </div>
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo">
-                                                Collapsible Group Item #2
-                                            </a>
-                                        </div>
-                                        <div id="collapseTwo" class="collapse" data-parent="#accordion">
-                                            <div class="card-body">
-                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <a class="collapsed card-link" data-toggle="collapse" href="#collapseThree">
-                                                Collapsible Group Item #3
-                                            </a>
-                                        </div>
-                                        <div id="collapseThree" class="collapse" data-parent="#accordion">
-                                            <div class="card-body">
-                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                            </div>
-                                        </div>
-                                    </div><!--end class card-->
+                                    <!--div class="card"-->
+                                    <!--div class="card-header"-->
+                                        <!--a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo"-->
+                                            
+                                        <!--/a-->
+                                    <!--/div-->
+                                    <!--div id="collapseTwo" class="collapse" data-parent="#accordion"-->
+                                        <!--div class="card-body"-->
+                                            
+                                        <!--/div-->
+                                    <!--/div-->
+                                <!--/div-->
+                                <!--div class="card"-->
+                                    <!--div class="card-header"-->
+                                        <!--a class="collapsed card-link" data-toggle="collapse" href="#collapseThree"-->
+                                            
+                                        <!--/a-->
+                                    <!--/div-->
+                                    <!--div id="collapseThree" class="collapse" data-parent="#accordion"-->
+                                        <!--div class="card-body"-->
+                                            
+                                        <!--/div-->
+                                    <!--/div-->
+                                <!--/div--><!--end class card-->
                             </div>
                         </div>
                     </section>
